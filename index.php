928f1af8b801f75d5c1352ef37d1a6516684f8b3
ARI-marker <arisheqiri833@gmail.com>
2025-05-14 19:15:43 +0200

    babav

--- index.php
+++ index.php
@@ -1,13 +1,70 @@
 <?php
 
-function maximum(){
+function maximum($x , $y){
 
-    ($x < $y)
-
-
-
-
+    if($x > $y){
+        return $x;
+    }else{
+        return $y;
+    }
 }
+
+$a = 20;
+$b = 30;
+
+$test = maximum($a , $b);
+echo "Max of $a and $b is $test";
+
+echo "<br>";
+
+function divisible($n){
+
+    if(($n % 7)== 0){
+        return "$n is fully divisible by 2";
+    }
+    else{
+        return "$n is not fully divisible by 2";
+    }
+}
+
+print_r(divisible(4)."<br>");
+print_r(divisible(35). "<br>");
+print_r(divisible(16). "<br>");
+print_r(divisible(21). "<br>");
+
+echo "<br>";
+
+$sports = ["Football","Tenis","Rockey","Basketball","Volleyball"];
+
+echo $sports[0];
+
+echo "<br>";
+
+echo end($sports);
+
+echo "<br>";
+
+echo count($sports);
+
+echo "<br>";
+
+
+
+
+
+
+
+
+ 
+
+
+    
+
+
+
+
+
+
 
 
 
